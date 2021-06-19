eb0c934c13d75d9cf207cc421248302f13e8a0d7
Ardhendu <ardhendujj002@gmail.com>
2021-06-19 20:09:48 +0530

    Added level3 authentication using hashing technique used md5 npm module

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
+const md5 = require('md5');
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const  encrypt = require('mongoose-encryption');
+const encrypt = require('mongoose-encryption');
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -15,8 +16,8 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
+console.log(md5('789'));
 const User = new mongoose.model("User",userSchema);
 app.get("/", function(req,res){
   res.render("home");
@@ -33,7 +34,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     username:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   })
   newUser.save(function(err){
     if(!err){
@@ -45,7 +46,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({username:username},function(err, found){
     if(err){
